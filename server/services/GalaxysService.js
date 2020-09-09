2fc85f3be91a0092ab540492a1055ae0f0f6b248
Nehemiah Boone <nehemiah.boone@gmail.com>
2020-09-09 13:45:24 -0600

    galaxy service done

--- server/services/GalaxysService.js
+++ server/services/GalaxysService.js
@@ -8,19 +8,27 @@ class GalaxysService {
   }
 
   async findById(id) {
-
+    let galaxy = await dbContext.Galaxys.findById(id)
+    return galaxy
   }
 
   async create(body) {
-
+    return await dbContext.Galaxys.create(body)
   }
 
   async edit(body) {
-
+    let update = await dbContext.Galaxys.findOneAndUpdate({ _id: body.id }, body, { new: true })
+    if (!update) {
+      throw new BadRequest("Invalid id")
+    }
+    return update
   }
 
   async delete(id) {
-
+    let success = await dbContext.Galaxys.findByIdAndDelete(id)
+    if (!success) {
+      throw new BadRequest("Invalid id")
+    }
   }
 }
 
