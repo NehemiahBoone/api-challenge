a242e792b85771176a1ca8ce7f5674c51b29e901
Nehemiah Boone <nehemiah.boone@gmail.com>
2020-09-09 09:39:42 -0600

    more setup

--- server/controllers/CatsController.js
+++ server/controllers/CatsController.js
@@ -7,20 +7,55 @@ export class CatsController extends BaseController {
     super("api/cats");
     this.router
       .get("", this.getAll)
-      .post("", this.create);
+      .get("/:id", this.getById)
+      .post("", this.create)
+      .put("/:id", this.edit)
+      .delete("/:id", this.delete)
   }
-  async getAll(_, res, next) {
+
+  async getAll(req, res, next) {
     try {
-      return res.send(["value1", "value2"]);
+      let data = await catsService.getAll(req.query)
+      res.send(data)
     } catch (error) {
       next(error);
     }
   }
+
+  async getById(req, res, next) {
+    try {
+      let data = await catsService.getById(req.params.id)
+      res.send(data)
+    } catch (error) {
+      next(error)
+    }
+  }
+
   async create(req, res, next) {
     try {
-      res.send(req.body);
+      let data = await catsService.create(req.body)
+      res.send(data);
     } catch (error) {
       next(error);
+    }
+  }
+
+  async edit(req, res, next) {
+    try {
+      req.body.id = req.params.id
+      let data = await catsService.edit(req.body)
+      res.send(data)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async delete(req, res, next) {
+    try {
+      await catsService.delete(req.params.id)
+      res.send("Successfully deleted")
+    } catch (error) {
+      next(error)
     }
   }
 }
--- server/services/CatsService.js
+++ server/services/CatsService.js
@@ -2,16 +2,29 @@ import { dbContext } from "../db/DbContext";
 import { BadRequest } from "../utils/Errors";
 
 class CatsService {
-  async find(query = {}) {
+  async getAll(query = {}) {
     let cats = await dbContext.Cats.find(query);
     return cats;
   }
-  async findById(id) {
+
+  async getById(id) {
     let cat = await dbContext.Cats.findById(id);
     if (!cat) {
       throw new BadRequest("Invalid Id");
     }
     return cat;
+  }
+
+  async create(body) {
+    throw new Error("Method not implemented.");
+  }
+
+  async edit(body) {
+    throw new Error("Method not implemented.");
+  }
+
+  async delete(id) {
+    throw new Error("Method not implemented.");
   }
 }
 
